8c1e1ea6b75b24b0043ae6b45dae08e17eb3ec8d
liujinping <853982366@qq.com>
2016-12-19 18:11:26 +0800

    AppContext.java文件复原

--- src/main/java/com/shubo/AppContext.java
+++ src/main/java/com/shubo/AppContext.java
@@ -10,13 +10,13 @@ import java.util.Date;
 public class AppContext {
 
     public static final String TABLE_OUTPUT_DIR = "表格";
-    //public static final String JSON_OUTPUT_DIR = "JSON";
-    public static final String JSON_OUTPUT_DIR = "JSONtest";//测试用
+    public static final String JSON_OUTPUT_DIR = "JSON";
+    //public static final String JSON_OUTPUT_DIR = "JSONtest";//测试用
     public static final String YEAR_REPORT_FOLDER = "年度报告";
 
     public static final String yearReportFolder = AppContext.rootFolder + File.separator + AppContext.YEAR_REPORT_FOLDER;
     public static String rootFolder = "e:\\年报解析";
-    //public static String srcFolder = rootFolder + File.separator + "html";
-    public static String srcFolder = rootFolder + File.separator + "年报";//测试用
+    public static String srcFolder = rootFolder + File.separator + "html";
+    //public static String srcFolder = rootFolder + File.separator + "年报";//测试用
     public static String dateStr = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
 }
