ba34fdd599062bfa1b3fcd6ba3c0571da67692c9
liujinping <853982366@qq.com>
2016-12-22 14:46:50 +0800

    文件下会产生null文件夹bug修复

--- src/main/java/com/shubo/AppContext.java
+++ src/main/java/com/shubo/AppContext.java
@@ -1,8 +1,6 @@
 package com.shubo;
 
 import java.io.File;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 /**
  * Created by horseman on 2016/11/25.
@@ -10,23 +8,21 @@ import java.util.Date;
 public class AppContext {
 
     public static final String TABLE_OUTPUT_DIR = "表格";
-    //public static final String JSON_OUTPUT_DIR = "JSON";
-    public static final String JSON_OUTPUT_DIR = "JSONtest";//测试用
+    public static final String JSON_OUTPUT_DIR = "JSON";
+    //public static final String JSON_OUTPUT_DIR = "JSONtest";//测试用
     public static final String YEAR_REPORT_FOLDER = "年度报告";
 
     public static final String MATCH_FAILD_FOLDER = "字段匹配失败";
-
-    public static final String yearReportFolder = AppContext.rootFolder + File.separator + AppContext.YEAR_REPORT_FOLDER;
-
-    public static final String matchFaildFolder = AppContext.rootFolder + File.separator + AppContext.MATCH_FAILD_FOLDER;
 
     public static void setRootFolder(String folder) {
         rootFolder = folder;
         srcFolder = rootFolder + File.separator + "html";
     }
 
-    public static String rootFolder = "e:\\年报解析2";
-    //public static String rootFolder = "e:\\年报解析";
-    //public static String srcFolder = rootFolder + File.separator + "html";
-    public static String srcFolder = rootFolder + File.separator + "年报";//测试用
+    //public static String rootFolder = "e:\\年报解析2";
+    public static String rootFolder = "e:\\年报解析";
+    public static String srcFolder = rootFolder + File.separator + "html";
+    //public static String srcFolder = rootFolder + File.separator + "年报";//测试用
+    public static String yearReportFolder = AppContext.rootFolder + File.separator + YEAR_REPORT_FOLDER;
+    public static String matchFaildFolder = AppContext.rootFolder + File.separator + MATCH_FAILD_FOLDER;
 }
